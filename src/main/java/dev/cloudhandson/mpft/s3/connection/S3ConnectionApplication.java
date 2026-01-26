77ae1e4733aaec69f2e7f564da24a35cd1815b7f
cloudhandson.dev <cloudhandson.dev@gmail.com>
2026-01-26 14:08:52 +0000

    Added Spring Boot setup

---
+++ src/main/java/dev/cloudhandson/mpft/s3/connection/S3ConnectionApplication.java
@@ -0,0 +1,13 @@
+package dev.cloudhandson.mpft.s3.connection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3ConnectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S3ConnectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/cloudhandson/mpft/s3/connection/S3ConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cloudhandson.mpft.s3.connection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3ConnectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
